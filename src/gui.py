2c9bae7773461c7775ef308488b0c134b53a8005
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-09-07 19:51:13 +0200

    Beginning of the GUI

--- src/gui.py
+++ src/gui.py
@@ -1,2 +1,19 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import classes
+from tkinter import *
+
+# main window
+window = Tk()
+
+# title of the game
+title = Label(window, text="Minesweeper")
+title.pack(side="top")
+
+# frame in which the buttons will be displayed
+frame = Frame(window, borderwidth=10)
+frame.pack(fill=BOTH)
+button = Button(frame, command=window.quit)
+button.pack()
+window.mainloop()
